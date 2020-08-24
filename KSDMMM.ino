ecf2a33980692ede37e31d11dc4e2f301962fd06
Joe Jackson <jbudorg@gmail.com>
2020-08-24 05:16:34 -0700

    initial commit v0.9.1b
    initial commit of 0.9.1b

---
+++ KSDMMM.ino
@@ -0,0 +1,198 @@
+
+#include <EEPROM.h>
+
+// comment this out when deployed:
+#define DEBUG
+
+// beta stuff
+//#define BETA
+
+
+// some arduinos have issues with pulling ground to some pins upon startup.
+#define ERROR_OFFSET 1
+
+// Set time to wait between inputs in ms (recomended 500 or greater)
+#define INPUT_WAIT_TIME 500
+// Set time to wait before outputing memory to vehicle in ms (recommended 2000-5000)
+#define STARTUP_WAIT_TIME 5000
+
+#ifdef DEBUG
+ #define DEBUG_PRINT(x)  Serial.println (x)
+#else
+ #define DEBUG_PRINT(x)
+#endif
+
+int memAddress = 0; 
+int memSetupAddr = 1;
+int memFuture1Addr = 2;
+
+const byte SMARTMODE = 0;
+const byte ECOMODE = 1;
+const byte COMFORTMODE = 2;
+const byte SPORTMODE = 3;
+const byte CUSTOMMODE = 4;
+
+byte currentMode;
+#ifdef BETA
+bool isg = true;
+#endif
+unsigned long t = 0; // current time
+unsigned long p = 0; // previous time
+
+
+void setup() {
+  pinMode(3, INPUT_PULLUP); // clockwise input
+  pinMode(4, INPUT_PULLUP); // counterclockwise input
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+#ifdef BETA
+  pinMode(7, INPUT_PULLUP); // ISG (Auto Start Stop) input
+  pinMode(8, OUTPUT);
+  if (!EEPROM.read(memFuture1Addr)) {
+    isg = false; // disable ISG
+  }
+#endif
+  //If initial setup hasn't completed, set the default drive mode
+  if (EEPROM.read(memSetupAddr) != 1) { 
+    // Write the default to permanent memory
+    EEPROM.write(memAddress, COMFORTMODE);
+    EEPROM.write(memSetupAddr, 1);
+    currentMode = COMFORTMODE;
+  } else {
+    // Setup has completed read the current mode from permanent memory
+    currentMode = EEPROM.read(memAddress); 
+  }
+#ifdef DEBUG
+  Serial.begin(9600);
+#endif
+
+  bool modechanged = true;
+  delay(STARTUP_WAIT_TIME);
+  switch(currentMode){
+      case SPORTMODE:
+        clockWise(1-ERROR_OFFSET);
+        break;
+      case CUSTOMMODE:
+        clockWise(2-ERROR_OFFSET);
+        break;
+      case SMARTMODE:
+        counterClockWise(2+ERROR_OFFSET);
+        break;
+      case COMFORTMODE:
+        counterClockWise(ERROR_OFFSET);
+        break;
+      case ECOMODE:
+        counterClockWise(ERROR_OFFSET);
+        break;
+      default:
+        modechanged = false;
+        break;
+  }
+  if (modechanged){
+    DEBUG_PRINT("mode has changed.");
+  }
+#ifdef BETA
+  // if ISG is disabled in memory, send a signal. ISG is on by default.
+  if (!isg){
+    digitalWrite(8, HIGH);
+    delay(250);
+    digitalWrite(8, LOW);
+    DEBUG_PRINT("ISG Disabled");
+  }
+#endif
+  char s[50];
+  sprintf(s, "setup mode: %s", modeText(currentMode));
+  DEBUG_PRINT(s);
+}
+
+void loop() {
+  char c[50];
+  // constantly read the input pins increment/decrement 
+  // the drive mode and store it in permanent memory
+  if (digitalRead(4) == LOW){
+    t = millis(); // get current arduino uptime.
+    if (t -  INPUT_WAIT_TIME > p){ // if 1 second has bassed since last reading
+      if (currentMode >= CUSTOMMODE){
+        currentMode = CUSTOMMODE; 
+      } else {
+        currentMode++;
+      }
+      sprintf(c, "Clock Triggered, Current Mode: %s", modeText(currentMode));
+      EEPROM.write(memAddress, currentMode);
+      DEBUG_PRINT(c);
+      p = t; // last reading is now, set p(revious) to current time
+    }
+  }
+  if (digitalRead(3) == LOW){
+    t = millis(); // get current arduino uptime.
+    if (t - INPUT_WAIT_TIME > p){ // if 1 second has bassed since last reading
+      if (currentMode <= SMARTMODE){
+        currentMode = SMARTMODE; 
+      } else {
+        currentMode--;
+      }
+      sprintf(c, "CClock Triggered, Current Mode: %s", modeText(currentMode));
+      EEPROM.write(memAddress, currentMode);
+      DEBUG_PRINT(c);
+      p = t; // last reading is now, set p(revious) to current time
+    }
+  }
+#ifdef BETA
+  if (digitalRead(7) == LOW){
+    t = millis(); // get current arduino uptime.
+    if (t -  INPUT_WAIT_TIME > p){ // if 1 second has bassed since last reading
+      isg != isg;
+      sprintf(c, "ISG Triggered, Current Mode: %s", isg ? "on" : "off");
+      EEPROM.write(memFuture1, isg);
+      DEBUG_PRINT(c);
+      p = t; // last reading is now, set p(revious) to current time
+    }
+  }
+#endif
+}
+
+// simulate a clockwise turn of drive mode num times
+void clockWise(int num){
+  for(int i=0;i<num;i++){
+    digitalWrite(5, HIGH);
+    delay(250);
+    digitalWrite(5, LOW);
+    delay(250);
+    DEBUG_PRINT("clockset");
+  }
+}
+
+// simulate a counterclockwise turn of drive mode num times
+void counterClockWise(int num){
+  for(int i=0;i<num;i++){
+    digitalWrite(6, HIGH);
+    delay(250);
+    digitalWrite(6, LOW);
+    delay(250);
+    DEBUG_PRINT("cclockset");
+  }
+}
+
+char* modeText(byte s){
+  char* r;
+  switch(s) {
+    case SMARTMODE:
+      r = "Smart";
+      break;
+    case ECOMODE:
+      r = "ECO";
+      break;
+    case COMFORTMODE:
+      r = "Comfort";
+      break;
+    case SPORTMODE:
+      r = "Sport";
+      break;
+    case CUSTOMMODE:
+      r = "Custom";
+      break;
+     default:
+      r = "";
+  }
+  return r;
+}
